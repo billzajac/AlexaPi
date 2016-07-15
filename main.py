05a0d84337265100d55573ade18bf03b45585b24
William Zajac <william.zajac@disney.com>
2016-07-15 12:02:55 -0700

    now using arecord

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import pprint
 import datetime
 import commands
 import os
@@ -27,9 +28,6 @@ token = None
 
 global token_updated_at
 token_updated_at = datetime.datetime.now() - datetime.timedelta(hours = 2)
-
-global playback_subprocess_pid
-playback_subprocess_pid = None
 
 def wait_for_sound_hardware():
     print "Waiting until the sound card is ready"
@@ -78,20 +76,6 @@ def gettoken():
         return False
         
 def alexa():
-    # First ensure that we actually recorded something reasonable
-    # sox_process = subprocess.Popen('sox -t .s16 recording.wav -n stat 2>&1|grep Length|awk \'{print $3}\'', stdout=subprocess.PIPE)
-    # out, err = sox_process.communicate()
-    #try:
-    #    sox_process = subprocess.Popen(['/usr/bin/sox', '-t', '.s16', path+'recording.wav', '-n', 'stat'], stderr=subprocess.PIPE)
-    #    out, err = sox_process.communicate()
-    #    recording_length = float(re.findall(r'Length.*(\d+\.\d+)', err)[0])
-    #    if recording_length < 1:
-    #        print "Recording was too short: {}".format(recording_length)
-    #        return
-    #except:
-    #    print "Failed to determine the length of the recording"
-    #    return
-
     url = 'https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize'
     headers = {'Authorization' : 'Bearer %s' % gettoken()}
     d = {
@@ -114,6 +98,7 @@ def alexa():
             "format": "audio/L16; rate=16000; channels=1"
         }
     }
+    print('Sending request to Amazon')
     with open(path+'recording.wav') as inf:
         files = [
                 ('file', ('request', json.dumps(d), 'application/json; charset=UTF-8')),
@@ -121,6 +106,7 @@ def alexa():
                 ]   
         r = requests.post(url, headers=headers, files=files)
     if r.status_code == 200:
+        print('Successful post to Amazon')
         for v in r.headers['content-type'].split(";"):
             if re.match('.*boundary.*', v):
                 boundary =  v.split("=")[1]
@@ -130,72 +116,34 @@ def alexa():
                 audio = d.split('\r\n\r\n')[1].rstrip('--')
         with open(path+"response.mp3", 'wb') as f:
             f.write(audio)
-        #os.system('play -q {}1sec.mp3 {}response.mp3'.format(path, path))
-        #subprocess.call(['play', '-q', '{}1sec.mp3'.format(path), '{}response.mp3'.format(path)])
-        # The os.setsid() is passed in the argument preexec_fn so it's run after the fork() and before  exec() to run the shell.
-        #playback_subprocess = subprocess.Popen('play -q {}1sec.mp3 {}response.mp3'.format(path, path), close_fds=True, shell=True, preexec_fn=os.setsid)
-        global playback_subprocess
-        #playback_subprocess = subprocess.Popen('play -q {}1sec.mp3 {}response.mp3'.format(path, path), shell=True, preexec_fn=os.setsid)
-        playback_subprocess = subprocess.Popen('lame --decode {}response.mp3 - | aplay -'.format(path), shell=True, preexec_fn=os.setsid)
+        print('Playing {}response.mp3').format(path)
+        subprocess.call('lame --decode {}response.mp3 - | aplay -'.format(path), shell=True)
+    else:
+      print('Unsuccessful post to Amazon: return code: {}').format(r.status_code)
+      pp = pprint.PrettyPrinter(indent=4)
+      pp.pprint(r.json())
+
 
 def button_pressed():
     print "Button Pressed: {}".format(time.strftime("%H:%M:%S"))
+    # We will fork the arecord process to the background and wait until the button is released to kill it
 
-    # Ensure that we don't allow ghost button presses
-    # I often find a bounce about 2 seconds after the initial
-    #global last_button_release
-    #if last_button_release:
-    #    if (datetime.datetime.now() - last_button_release).total_seconds() > 2:
-    #        last_button_release = None
-    #    else:
-    #        print "Button press too close, probably a bounce.  Ignoring."
-    #        return
-
-    global playback_subprocess_pid
-    if playback_subprocess_pid:
-        print "Subprocess PID exists: {} Let's kill it!".format(playback_subprocess_pid)
-        try:
-            # source: http://stackoverflow.com/questions/4789837/how-to-terminate-a-python-subprocess-launched-with-shell-true
-            process_group_id = os.getpgid(playback_subprocess_pid)
-            print "Killing subprocess group ID: {}".format(process_group_id)
-            os.killpg(process_group_id, signal.SIGKILL) # send the signal to all the process groups (SIGKILL/SIGTERM)
-            print "Successful termination!"
-        except:
-            print "Failed to terminate process"
-    # print "No process running to terminate"
-
-    record_and_process()
-
-# TODO: figure out why this is not recording
-def record_and_process():
-    inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, device)
-    inp.setchannels(1)
-    inp.setrate(16000)
-    inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-    inp.setperiodsize(500)
-    audio = ""
-    l, data = inp.read()
-    if l:
-        audio += data
+    # The os.setsid() is passed in the argument preexec_fn so it's run after the fork() and before  exec() to run the shell.
+    record_subprocess = subprocess.Popen('arecord -f S16_LE -c 1 -r 16000 recording.wav', shell=True, preexec_fn=os.setsid)
+    record_subprocess_pid = record_subprocess.pid
+    record_subprocess_group_id = os.getpgid(record_subprocess_pid)
 
     status, output = commands.getstatusoutput('/usr/local/lb/Button/bin/getButton')
     while status != 0:
-        l, data = inp.read()
-        if l:
-            audio += data
-        status, output = commands.getstatusoutput('/usr/local/lb/Button/bin/getButton')
+            status, output = commands.getstatusoutput('/usr/local/lb/Button/bin/getButton')
+            time.sleep(0.2)
+    os.killpg(record_subprocess_group_id, signal.SIGKILL) # send the signal to all the process groups (SIGKILL/SIGTERM)
+    # why get fancy.  let's go crazy
+    subprocess.call(['killall', 'arecord'])
 
     # Button is up now
     print "Button Released: {}".format(time.strftime("%H:%M:%S"))
 
-    # This is used to ensure that we don't allow ghost button presses
-    # I often find a bounce about 2 seconds after the initial
-    #global last_button_release
-    #last_button_release = datetime.datetime.now()
-
-    rf = open(path+'recording.wav', 'w') 
-    rf.write(audio)
-    rf.close()
     alexa()
 
 if __name__ == "__main__":
